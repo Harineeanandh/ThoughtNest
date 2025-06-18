64956c91c34fe92b2f3f716134082c88f3459afc
Harineeanandh <your-email@example.com>
2025-06-19 04:26:00 +0530

    Refactor GCSUploadService to use default credentials for Cloud Run

--- ThoughtNestBackend/src/main/java/com/thoughtNest/backend/service/GCSUploadService.java
+++ ThoughtNestBackend/src/main/java/com/thoughtNest/backend/service/GCSUploadService.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.Acl;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
@@ -20,32 +19,20 @@ public class GCSUploadService {
     @Value("${gcs.bucket.name}")
     private String bucketName;
 
-    @Value("${gcs.credentials.file}")
-    private String credentialsPath;
-
     public String uploadFile(String originalFilename, InputStream inputStream, String contentType) throws IOException {
-    String fileName = UUID.randomUUID() + "-" + originalFilename;
+        String fileName = UUID.randomUUID() + "-" + originalFilename;
 
-    // Load credentials from classpath
-    InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream(credentialsPath);
-    if (credentialsStream == null) {
-        throw new IOException("GCS credentials file not found in classpath: " + credentialsPath);
+        // Use default credentials provided by Google Cloud Run
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+
+        BlobId blobId = BlobId.of(bucketName, fileName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                .setContentType(contentType)
+                .setAcl(java.util.List.of(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))) // Make public
+                .build();
+
+        storage.create(blobInfo, inputStream);
+
+        return String.format("https://storage.googleapis.com/%s/%s", bucketName, fileName);
     }
-
-    Storage storage = StorageOptions.newBuilder()
-            .setCredentials(ServiceAccountCredentials.fromStream(credentialsStream))
-            .build()
-            .getService();
-
-    BlobId blobId = BlobId.of(bucketName, fileName);
-    BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
-            .setContentType(contentType)
-            .setAcl(java.util.List.of(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))) // Public ACL
-            .build();
-
-    storage.create(blobInfo, inputStream);
-
-    return String.format("https://storage.googleapis.com/%s/%s", bucketName, fileName);
-}
-
 }
